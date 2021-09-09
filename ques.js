e732ab0f68743385086441daa2dbe9fe3ec2528c
Kamron3519 <qwertysaidamirxon97@gmail.com>
2021-09-09 19:39:30 +0500

    Uploaded quiz test

---
+++ ques.js
@@ -0,0 +1,58 @@
+// creating an the passing the number, questions, options and answers
+let questions = [
+    {
+        numb:1,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        option:[
+                "Hyper Text Proprocessor",
+                "Hyper Text Markup Language",
+                "Hyper Text Multiple Language",
+                "Hyper Tool Multi Language"
+            ]
+    },
+    {
+        numb:2,
+        question:"What does CSS stand for?",
+        answer:"Cascading Style Sheet",
+        option:[
+                "Common Style Sheet",
+                "Colorful Style Sheet",
+                "Computer Style Sheet",
+                "Cascading Style Sheet"
+            ]
+    },
+    {
+        numb:3,
+        question:"What does PHP stand for?",
+        answer:"Hypertext Proprocessor",
+        option:[
+                "Hypertext Proprocessor",
+                "Hypertext Programming",
+                "Hypertext Proprogramming",
+                "Hometext Proprocessor"
+            ]
+    },
+    {
+        numb:4,
+        question:"What does SQL stand for?",
+        answer:"Structured Query Language",
+        option:[
+                "Stylish Question Language",
+                "Stylesheet Query Language",
+                "Statement question Language",
+                "Structured Query Language"
+            ]
+    },
+    {
+        numb:5,
+        question:"What does XML stand for?",
+        answer:"extensible Markup Language",
+        option:[
+                "extensible Markup Language",
+                "eXecutable Multiple Language",
+                "eXtra Multi-Program Language",
+                "eXamine MUltiple Language"
+            ]
+    },
+]
